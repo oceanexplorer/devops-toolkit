60473a065556e3fd0cd99a6e56cbe71c06067d0f
Paul Custance <pcustance@gmail.com>
2017-10-22 14:56:09 +0100

    Added scripts currently in use

---
+++ cloud-provisioning/scripts/dm-test-swarm.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ "$(uname -s )" == "Linux" ]]; then
+  export VIRTUALBOX_SHARE_FOLDER="$PWD:$PWD"
+fi
+
+for i in 1 2 3; do
+    docker-machine create \
+        -d virtualbox \
+        --virtualbox-memory 512 \
+        swarm-test-$i
+done
+
+eval $(docker-machine env swarm-test-1)
+
+docker swarm init \
+    --advertise-addr $(docker-machine ip swarm-test-1)
+
+TOKEN=$(docker swarm join-token -q manager)
+
+for i in 2 3; do
+    eval $(docker-machine env swarm-test-$i)
+
+    docker swarm join \
+        --token $TOKEN \
+        --advertise-addr $(docker-machine ip swarm-test-$i) \
+        $(docker-machine ip swarm-test-1):2377
+done
+
+echo ">> The swarm test cluster is up and running"
