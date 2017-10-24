1457b44cdb0526142187d30efa9f2278e6fad6f8
Paul Custance <pcustance@gmail.com>
2017-10-24 21:48:37 +0100

    Added production swarm services script, corrected an error in the test swarm services script and added a script for generating the test swarm cluster.

--- cloud-provisioning/scripts/dm-swarm-services.sh
+++ cloud-provisioning/scripts/dm-swarm-services.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-eval $(docker-machine env swarm-test-1)
+eval $(docker-machine env swarm-1)
 
 docker network create --driver overlay proxy
 
@@ -8,16 +8,16 @@ docker network create --driver overlay todoapi
 
 curl -o docker-compose-proxy.yml https://raw.githubusercontent.com/vfarcic/docker-flow-proxy/master/docker-compose.yml
 
-export DOCKER_IP=$(docker-machine ip swarm-test-1)
+export DOCKER_IP=$(docker-machine ip swarm-1)
 
 docker-compose -f docker-compose-proxy.yml up -d consul-server
 
-export CONSUL_SERVER_IP=$(docker-machine ip swarm-test-1)
+export CONSUL_SERVER_IP=$(docker-machine ip swarm-1)
 
 for i in 2 3; do
-    eval $(docker-machine env swarm-test-$i)
+    eval $(docker-machine env swarm-$i)
 
-    export DOCKER_IP=$(docker-machine ip swarm-test-$i)
+    export DOCKER_IP=$(docker-machine ip swarm-$i)
 
     docker-compose -f docker-compose-proxy.yml up -d consul-agent
 done
@@ -30,21 +30,21 @@ docker service create --name proxy \
     -p 8090:8080 \
     --network proxy \
     -e MODE=swarm \
-    --replicas 2 \
-    -e CONSUL_ADDRESS="$(docker-machine ip swarm-test-1):8500,$(docker-machine ip swarm-test-2):8500,$(docker-machine ip swarm-test-3):8500" \
-    --constraint 'node.labels.env == prod-like' \
+    --replicas 3 \
+    -e CONSUL_ADDRESS="$(docker-machine ip swarm-1):8500,$(docker-machine ip swarm-2):8500,$(docker-machine ip swarm-3):8500" \
+    --reserve-memory 50m \
     vfarcic/docker-flow-proxy
 
 docker service create --name todoapi-db \
     --network todoapi \
-    --constraint 'node.labels.env == prod-like' \
+    --reserve-memory 150m \
     -e POSTGRES_PASSWORD=Testing123 \
     postgres
 
 while true; do
     REPLICAS=$(docker service ls | grep proxy | awk '{print $3}')
     REPLICAS_NEW=$(docker service ls | grep proxy | awk '{print $4}')
-    if [[ $REPLICAS == "2/2" || $REPLICAS_NEW == "2/2" ]]; then
+    if [[ $REPLICAS == "3/3" || $REPLICAS_NEW == "3/3" ]]; then
         break
     else
         echo "Waiting for the proxy service..."
@@ -68,15 +68,15 @@ docker service create --name todoapi \
     -e DATABASE_SA_PASSWORD=Testing123 \
     --network todoapi \
     --network proxy \
-    --replicas 2 \
-    --constraint 'node.labels.env == prod-like' \
+    --replicas 3 \
+    --reserve-memory 50m \
     --update-delay 5s \
     oceanexplorer/todoapi:latest
 
 while true; do
     REPLICAS=$(docker service ls | grep oceanexplorer/todoapi | awk '{print $3}')
     REPLICAS_NEW=$(docker service ls | grep oceanexplorer/todoapi | awk '{print $4}')
-    if [[ $REPLICAS == "2/2" || $REPLICAS_NEW == "2/2" ]]; then
+    if [[ $REPLICAS == "3/3" || $REPLICAS_NEW == "3/3" ]]; then
         break
     else
         echo "Waiting for the todoapi service..."
@@ -84,7 +84,7 @@ while true; do
     fi
 done
 
-curl "$(docker-machine ip swarm-test-1):8090/v1/docker-flow-proxy/reconfigure?serviceName=todoapi&servicePath=/api&port=5050&distribute=true"
+curl "$(docker-machine ip swarm-1):8090/v1/docker-flow-proxy/reconfigure?serviceName=todoapi&servicePath=/api&port=5050&distribute=true"
 
 echo ""
-echo ">> The services are up and running inside the swarm test cluster"
+echo ">> The services are up and running inside the swarm cluster"
